6151ccd14882473b1bf5305279f8208697a1e1e4
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-04-25 16:34:18 +0700

    add solution for excersice 1.6-1.7

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+const Title = props => <h2>{props.text}</h2>
+
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+  }
+
+  const averageScore = () => {
+    if(total === 0){
+      return 0
+    }
+
+    return ((good * 1) + (neutral * 0) + (bad * -1)) / total
+  
+  }
+
+  return (
+    <div>
+      <Title text='give feedback' />
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <Title text='statistic' />
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {total}</p>
+      <p>Average {averageScore()}</p>
+    </div>
+  )
+}
+
+export default App
