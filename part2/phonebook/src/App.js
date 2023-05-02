0a94940ca71ca2a046c309039e2882bd8ee7ee2b
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-05-02 19:51:09 +0700

    add solution for excersise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from  'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import FilterPerson from './components/FilterPerson'
 
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
 
+  useEffect(() => {
+    console.log( 'effect')
+    axios
+    .get(' http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fullfilled')
+      setPersons(response.data)
+    })
+  }, [])
+
+  
   const addName = (event) => {
     event.preventDefault()
     const foundDuplicatedName = persons.find(person => person.name.toLowerCase() === newName.toLocaleLowerCase())
