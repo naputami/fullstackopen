701ce4bba6e296637dcc553aafd114192d7ca8ce
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-05-15 16:29:35 +0700

    add fix

---
+++ part3/README.md
@@ -0,0 +1,3 @@
+Live app: https://phonebookfullstack.cyclic.app
+
+Full repository for this excersise: https://github.com/naputami/phonebook-backend
--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('req-body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
@@ -92,7 +94,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
   
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
