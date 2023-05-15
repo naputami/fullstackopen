0045e3e08b479664aa29e91a99bde1de0c6087e0
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-05-15 21:36:22 +0700

    add solution for 4.1-4.2

---
+++ part4/app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controlers/blogs')
+const middleWare = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+})
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleWare.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleWare.unknownEndpoint)
+app.use(middleWare.errorHandler)
+
+module.exports = app
